ad097db6ab3ecfd06c3ab221730c7d3c50641ba7
Sachin Moodley <sachinmoodley@gmail.com>
2020-02-29 11:33:04 +0200

    wip: demo linked list

---
+++ DataStructures/LinkedListApp/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.LinkedListApp
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
