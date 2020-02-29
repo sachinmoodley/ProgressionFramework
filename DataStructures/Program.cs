445d479c270f9a2535723fdd3b03a5318f853bcb
Sachin Moodley <sachinmoodley@gmail.com>
2020-02-29 08:31:18 +0200

    add examples for a queue

---
+++ DataStructures/Program.cs
@@ -0,0 +1,16 @@
+﻿using DataStructures.QueueApp;
+using System;
+
+namespace DataStructures
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var licensingOffice = new LicensingOffice();
+            var customersEntered = licensingOffice.EnterCustomersInQueue();
+
+            licensingOffice.CustomersExitingTheQueue(customersEntered);
+        }
+    }
+}
---
+++ DataStructures/QueueApp/LicensingOffice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.QueueApp
+{
+    public class LicensingOffice
+    {
+        public Queue EnterCustomersInQueue()
+        {
+            var customerQueue = new Queue();
+            string customerName;
+            var queueLimit = 5;
+            for (int x = 0; x < queueLimit; x++)
+            {
+                Console.WriteLine("Please enter customer name?");
+                customerName = Console.ReadLine();
+                customerQueue.Enqueue(customerName);
+            }
+
+            Console.WriteLine("You have reached the maximum people in the Queue: " + customerQueue.Count);
+
+            foreach (string customer in customerQueue)
+            {
+                Console.WriteLine(customer);
+            }
+
+            return customerQueue;
+        }
+
+        public void CustomersExitingTheQueue(Queue customersInQueue)
+        {
+            foreach (string customer in customersInQueue)
+            {
+                Console.WriteLine("Customer exited: " + customer);
+            }
+        }
+    }
+}
