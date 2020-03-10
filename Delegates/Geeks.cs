a1c9cb0861d4145468852f3866c1ec248a1a6007
Sachin Moodley <sachinmoodley@gmail.com>
2020-03-10 19:15:22 +0200

    wip: delegates

---
+++ Delegates/Geeks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public class Geeks
+    {
+        public delegate void addnum(int a, int b);
+        public delegate void subnum(int a, int b);
+
+        // method "sum" 
+        public void sum(int a, int b)
+        {
+            Console.WriteLine("(100 + 40) = {0}", a + b);
+        }
+
+        // method "subtract" 
+        public void subtract(int a, int b)
+        {
+            Console.WriteLine("(100 - 60) = {0}", a - b);
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static Delegates.Geeks;
+
+namespace Delegates
+{
+
+    //example taken from https://www.geeksforgeeks.org/c-sharp-delegates/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Geeks();
+
+            addnum del_obj1 = new addnum(obj.sum);
+            subnum del_obj2 = new subnum(obj.subtract);
+
+            del_obj1(100, 40);
+            del_obj2(100, 60);
+
+            //Console.WriteLine(del_obj1.Invoke(100, 40));
+        }
+    }
+}
