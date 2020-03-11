6690e16006e30726dea1b604134fcbed5002a495
Sachin Moodley <sachin.moodley@chillisoft.co.za>
2020-03-11 15:16:22 +0200

    learning delegates

--- Delegates/Geeks.cs
+++ Delegates/Geeks.cs
@@ -20,5 +20,44 @@ namespace Delegates
         {
             Console.WriteLine("(100 - 60) = {0}", a - b);
         }
+
+        public delegate void InterestCalculationDelegate(int[] num);
+
+        public void CalculateInterest(int[] deposits, InterestCalculationDelegate calculateInterestCallback)
+        {
+            // Get interest rate
+            // Get term
+            // Get state of the country
+            // Get favorite color
+
+            calculateInterestCallback.Invoke(deposits);
+        }
+
+        public void CalculateAllInterestsForUser()
+        {
+            // Get user
+            // Validate user
+            // Get history
+            // Validate history
+            // Get employment records
+
+            var deposits = new[] {1, 2, 123, 45, 566, 2345, 677};
+
+            var calculateLongTerm = new InterestCalculationDelegate(CalculateLongTermInterest);
+            var calculateShortTerm = new InterestCalculationDelegate(CalculateShortTermInterest);
+
+            CalculateInterest(deposits, calculateLongTerm);
+            CalculateInterest(deposits, calculateShortTerm);
+        }
+
+        public void CalculateLongTermInterest(int[] num)
+        {
+            // Calculate very complicated stuff 
+        }        
+        
+        public void CalculateShortTermInterest(int[] num)
+        {
+            // Calculate other complicated stuff 
+        }
     }
 }
